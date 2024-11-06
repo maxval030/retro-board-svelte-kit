833248171408520bfae49fd80bb829d9019883a3
“NatMaxVal030” <overtakemadaka@gmail.com>
2024-11-07 04:28:54 +0700

    first commit 🎊

---
+++ src/routes/+page.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+	import { onMount, tick } from 'svelte';
+	import { Stage, Layer, Rect } from 'svelte-konva';
+	import type { KonvaDragTransformEvent } from 'svelte-konva';
+	type ConfigPositsType = {
+		x: number;
+		y: number;
+		width: number;
+		height: number;
+		stroke?: string;
+		strokeWidth?: number;
+		fill: string;
+		draggable: boolean;
+		id: string;
+	};
+	let color = $state('red');
+
+	let configPosits = $state<ConfigPositsType[]>([
+		{
+			x: 250,
+			y: 100,
+			width: 130,
+			height: 150,
+			// stroke: 'black',
+			// strokeWidth: 2,
+			fill: color,
+			draggable: true,
+			id: new Date().getTime().toString()
+		}
+	]);
+	// let yellowRingConfig = $state({
+	// 	x: 150,
+	// 	y: 150,
+	// 	innerRadius: 80,
+	// 	outerRadius: 120,
+	// 	fill: 'yellow',
+	// 	draggable: true
+	// });
+
+	function handlerDragend(event: KonvaDragTransformEvent) {
+		console.log('🚀 ~ moveEndHandler ~ event:', event);
+	}
+
+	function handlePositsChangeZIndex(event: KonvaDragTransformEvent) {
+		let target = event.target;
+
+		target.moveToTop();
+
+		target.on('dblclick', () => {
+			console.log('🚀 ~ target.on ~ dblclick');
+			let textPosition = target.getAbsolutePosition();
+			// console.log('🚀 ~ target.on ~ textPosition:', textPosition);
+
+			const a = document.getElementById('layer1');
+			console.log(a);
+			// let areaPosition = {
+			// 	x: window.innerWidth + textPosition.x,
+			// 	y: window.innerHeight + textPosition.y
+			// };
+
+			// let textarea = document.createElement('textarea');
+			// document.body.appendChild(textarea);
+			// then lets find position of stage container on the page:
+
+			// target.destroy();
+			// configPosits = configPosits.filter((item) => item.id !== target.attrs.id);
+		});
+	}
+
+	function addPosits() {
+		configPosits.push({
+			x: 250,
+			y: 100,
+			width: 130,
+			height: 150,
+			// stroke: 'black',
+			// strokeWidth: 2,
+			fill: color,
+			draggable: true,
+			id: new Date().getTime().toString()
+		});
+	}
+</script>
+
+<button onclick={addPosits}>Add posits</button>
+<button
+	onclick={() => {
+		color = '#fc8d59';
+	}}>Red</button
+>
+<button
+	onclick={() => {
+		color = '#e6f598';
+	}}>Blue</button
+>
+<div>
+	<Stage width={window.innerWidth} height={window.innerHeight}>
+		<Layer>
+			<!-- <Rect x={100} y={100} width={400} height={200} fill="blue" draggable /> -->
+			{#each configPosits as positsItem, i}
+				<Rect
+					cornerRadius={3}
+					{...positsItem}
+					ondragend={handlerDragend}
+					ondragstart={handlePositsChangeZIndex}
+					onclick={handlePositsChangeZIndex}
+				/>
+			{/each}
+		</Layer>
+	</Stage>
+</div>
