2e565c98469c4d944053128ec03dc9bfa76c9cda
“NatMaxVal030” <overtakemadaka@gmail.com>
2024-11-18 03:52:39 +0700

    feat: 🎸 add use env

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
-	import Button from "$lib/components/ui/button/button.svelte";
-
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { PUBLIC_END_POINT_POCKETBASE_URL } from '$env/static/public';
+	console.log(PUBLIC_END_POINT_POCKETBASE_URL);
 </script>
+
 <div>
-    <a href="/retro-board"><Button>Go to retro board</Button></a>
+	<a href="/retro-board"><Button>Go to retro board</Button></a>
 </div>
